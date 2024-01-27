1059781dbd923cd679ca75a38e80da780dfc04ae
David Bengui <davidbenguii@gmail.com>
2024-01-27 09:38:36 +0000

    Implementamos o hello world multi linguas

--- hello.py
+++ hello.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python3
-#Mais um comentario
+"""Hello world multi linguas.
 
-# Este programa imprime hello world
+Dependendo da lingua configurada no ambiente o programa
+exibe a mensagem correspondente.
 
-print ("Hello, World!")
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+  export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__="0.0.1"
+__author__="David Bengui"
+__licence__="unlicense"
+
+import os 
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World"
+
+if current_language =="pt_BR":
+	msg = "Ola, Mundo!"
+
+elif current_language =="it_IT":
+	msg = "Ciao, Mondo!"
+	
+print (msg)
