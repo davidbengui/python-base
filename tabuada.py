cfd8744654326e247cbafdfdd1ba579edaa59fe9
David Bengui <davidbenguii@gmail.com>
2024-01-30 15:34:09 +0000

    Alterada a formatacao da tabuada

--- tabuada.py
+++ tabuada.py
@@ -1,8 +1,26 @@
-#Tabuada dos numeros de 1 a 10
+"""
+__version__ = "0.1.1"
+__authot=__"david"
+
+template = 
+"""
+
+
 
 numeros = list(range(1,11))
-for numero in numeros:
-    print("Tabuada do numero",numero)
-    for novo_numero in numeros:
-        print(numero * novo_numero)
-    print("--------------")
+for n1 in numeros:
+    print("{:-^20}".format(f"Tabuada de Multiplicar do {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:-^18}".format(f"{n1} x {n2} = {resultado}"))
+         
+    print("#" * 18)
+
+for n1 in numeros:
+    print("{:-^20}".format(f"Soma do Nº {n1}".center(60)))
+    print()
+    for n2 in numeros:
+        soma = n1 + n2
+        print("{:-^18}".format(f"{n1} + {n2} ={soma}".center(60)))
+    print ("#" *70 )
